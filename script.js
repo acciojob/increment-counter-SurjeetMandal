32290d9bc506560a87b8e840b28f1a20002bfdd6
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-07 12:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var para = document.querySelector("#counter");
+var btn = document.querySelector("#incrementBtn");
+
+let counter = 0
+
+btn.addEventListener("click",function() {
+	alert(counter += 1)
+	para.innerHTML = counter;
+})
